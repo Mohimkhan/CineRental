ad369d473b3222d583022ba4e4c74473765d8669
Mohimkhan <mohimkhan652@gmail.com>
2024-01-22 12:43:32 +0600

    Add MovieList Component

---
+++ src/components/MovieList.jsx
@@ -0,0 +1,18 @@
+import { getAllMovies } from "../../data/movies.js";
+import MovieCart from "./MovieCart.jsx";
+
+const MovieList = () => {
+  const movies = getAllMovies();
+
+  return (
+    <div className="content">
+      <div className="grid sm:grid-cols-2 xl:grid-cols-3 gap-7">
+        {movies.map((movie) => (
+          <MovieCart key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MovieList;
