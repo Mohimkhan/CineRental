8bcfa3ead80d1d3b8d78d70f56917e3067066a99
Mohimkhan <mohimkhan652@gmail.com>
2024-01-22 22:35:44 +0600

    feat: toggle MovieDetailsModal Component

--- src/components/MovieCart.jsx
+++ src/components/MovieCart.jsx
@@ -1,31 +1,55 @@
+import { useState } from "react";
 import Image from "./Image";
 import Rating from "./Rating";
+import Link from "./Link";
+import MovieDetailsModal from "./MovieDetailsModal";
 import tagImage from "../assets/tag.svg";
 import { getImgUrl } from "../../utils/cine-utility.js";
 
+
 const MovieCart = ({ movie }) => {
+  const [showModal, setShowModal] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  function handleMovieSelection(movie) {
+    setSelectedMovie(movie);
+    setShowModal(true);
+  }
+
+  function handleModalClose() {
+    setSelectedMovie(null);
+    setShowModal(false);
+  }
+
   return (
-    <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
-      <Image
-        className="w-full object-cover"
-        imgSrc={getImgUrl(`${movie.cover}`)}
-        altText={movie.title}
-      />
-      <figcaption className="pt-4">
-        <h3 className="text-xl mb-1">{movie.title}</h3>
-        <p className="text-[#575A6E] text-sm mb-2">{movie.genre}</p>
-        <div className="flex items-center space-x-1 mb-5">
-          <Rating value={movie.rating} />
-        </div>
-        <a
-          className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
-          href="#"
-        >
-          <Image imgSrc={tagImage} altText="tag" />
-          <span>${movie.price} | Add to Cart</span>
-        </a>
-      </figcaption>
-    </figure>
+    <>
+      {showModal && (
+        <MovieDetailsModal movie={selectedMovie} onClose={handleModalClose} />
+      )}
+      <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
+        <Link to="#" onClick={() => handleMovieSelection(movie)}>
+          <Image
+            className="w-full object-cover"
+            imgSrc={getImgUrl(`${movie.cover}`)}
+            altText={movie.title}
+          />
+          <figcaption className="pt-4">
+            <h3 className="text-xl mb-1">{movie.title}</h3>
+            <p className="text-[#575A6E] text-sm mb-2">{movie.genre}</p>
+            <div className="flex items-center space-x-1 mb-5">
+              <Rating value={movie.rating} />
+            </div>
+            <button
+              className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
+              onClick={(e) => handleAddToCart(e, movie)}
+            >
+              <Image imgSrc={tagImage} altText="tag" />
+              <span>${movie.price} | Add to Cart</span>
+            </button>
+          </figcaption>
+        </Link>
+      </figure>
+    </>
   );
 };
 
