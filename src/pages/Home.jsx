33d7e301de909663171a1411fa1c40802f88a759
Mohimkhan <mohimkhan652@gmail.com>
2024-01-23 13:00:32 +0600

    refactor: maintain a cart state and pass it

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,20 +2,26 @@ import MovieList from "../components/MovieList";
 import Header from "../components/Header";
 import Sidebar from "../components/SideBar";
 import Footer from "../components/Footer";
+import { MovieProvider } from "../contexts/MoveContext";
+import { useState } from "react";
 
 const Home = () => {
+  const [cartData, setCartData] = useState([]);
+
   return (
     <>
-      <div className={`h-full w-full`}>
-        <Header />
-        <main>
-          <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
-            <Sidebar />
-            <MovieList />
-          </div>
-        </main>
-        <Footer />
-      </div>
+      <MovieProvider value={{ cartData, setCartData }}>
+        <div className={`h-full w-full`}>
+          <Header />
+          <main>
+            <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+              <Sidebar />
+              <MovieList />
+            </div>
+          </main>
+          <Footer />
+        </div>
+      </MovieProvider>
     </>
   );
 };
