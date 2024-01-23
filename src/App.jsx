0ebe710e3de15afb0602587058959da9f79ff5a0
Mohimkhan <mohimkhan652@gmail.com>
2024-01-23 20:19:34 +0600

    Update: pass all the movie and theme stae to all the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { useReducer } from "react";
+import { MovieProvider } from "./contexts/MoveContext";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import { cartReducer } from "./reducers/cart.js";
 import Home from "./pages/Home";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const initialState = [];
 
 function App() {
+  const [cartData, dispatch] = useReducer(cartReducer, initialState);
   return (
     <>
-      <Home />
+      <ThemeProvider>
+        <MovieProvider value={{ cartData, dispatch }}>
+          <Home />
+          <ToastContainer />
+        </MovieProvider>
+      </ThemeProvider>
     </>
   );
 }
