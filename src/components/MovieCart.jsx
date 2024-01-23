d3b86af18052fdc061ff5321c24ccfad7bc1215a
Mohimkhan <mohimkhan652@gmail.com>
2024-01-23 12:59:57 +0600

    feat: add to cart

--- src/components/MovieCart.jsx
+++ src/components/MovieCart.jsx
@@ -1,30 +1,47 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import Image from "./Image";
 import Rating from "./Rating";
 import Link from "./Link";
 import MovieDetailsModal from "./MovieDetailsModal";
 import tagImage from "../assets/tag.svg";
 import { getImgUrl } from "../../utils/cine-utility.js";
-
+import { MovieContext } from "../contexts/MoveContext.jsx";
 
 const MovieCart = ({ movie }) => {
   const [showModal, setShowModal] = useState(false);
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const { cartData, setCartData } = useContext(MovieContext);
 
-  function handleMovieSelection(movie) {
+  const handleMovieSelection = (movie) => {
     setSelectedMovie(movie);
     setShowModal(true);
-  }
+  };
 
-  function handleModalClose() {
+  const handleModalClose = () => {
     setSelectedMovie(null);
     setShowModal(false);
-  }
+  };
+
+  const handleAddToCart = (event, movie) => {
+    event.stopPropagation();
+
+    const found = cartData.find((item) => item.id === movie.id);
+
+    if (!found) {
+      setCartData((prev) => [...prev, movie]);
+    } else {
+      console.error(`The movie ${movie.title} is added to cart already!!!`);
+    }
+  };
 
   return (
     <>
       {showModal && (
-        <MovieDetailsModal movie={selectedMovie} onClose={handleModalClose} />
+        <MovieDetailsModal
+          movie={selectedMovie}
+          onClose={handleModalClose}
+          onCartAdd={handleAddToCart}
+        />
       )}
       <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
         <Link to="#" onClick={() => handleMovieSelection(movie)}>
