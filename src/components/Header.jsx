0ff9a5f98d6acfba0ece8d203831ca65a1c31b1e
Mohimkhan <mohimkhan652@gmail.com>
2024-01-23 12:49:13 +0600

    feat: toggle cart and show cart item count

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,21 @@
-import Image from './Image';
-import logo from '../assets/logo.svg';
-import ring from '../assets/ring.svg';
-import sun from '../assets/icons/sun.svg';
-import shoppingCart from '../assets/shopping-cart.svg';
-import Link from './Link';
+import { useContext, useState } from "react";
+import { MovieContext } from "../contexts/MoveContext";
+import Image from "./Image";
+import Link from "./Link";
+import CartDetails from "./CartDetails";
+import ring from "../assets/ring.svg";
+import logo from "../assets/logo.svg";
+import sun from "../assets/icons/sun.svg";
+import shoppingCart from "../assets/shopping-cart.svg";
+import Moon from "../assets/icons/moon.svg";
 
 const Header = () => {
+  const [showCart, setShowCart] = useState(false);
+  const {cartData, setCartData} = useContext(MovieContext);
+
   return (
     <header>
+      {showCart && <CartDetails onClose={() => setShowCart(false)} />}
       <nav className="container flex items-center justify-between space-x-10 py-6">
         <Link to="index.html">
           <Image imgSrc={logo} width="139" height="26" altText="logo" />
@@ -34,6 +42,7 @@ const Header = () => {
             <Link
               className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
               to="#"
+              onClick={() => setShowCart(true)}
             >
               <Image
                 imgSrc={shoppingCart}
@@ -41,6 +50,11 @@ const Header = () => {
                 height="24"
                 altText="shopping-cart"
               />
+              {cartData.length > 0 && (
+                <span className="rounded-full absolute top-[-12px] left-[28px] bg-[#12CF6F] text-white text-center p-[2px] w-[30px] h-[30px]">
+                  {cartData.length}
+                </span>
+              )}
             </Link>
           </li>
         </ul>
